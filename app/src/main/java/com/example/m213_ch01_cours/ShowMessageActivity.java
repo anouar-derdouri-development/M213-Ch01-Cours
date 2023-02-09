bd0d7f26e2384dba8f7cfb816a43eb8e03ee40e2
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-02-09 18:24:13 +0000

    Use Snackbar

--- app/src/main/java/com/example/m213_ch01_cours/ShowMessageActivity.java
+++ app/src/main/java/com/example/m213_ch01_cours/ShowMessageActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class ShowMessageActivity extends AppCompatActivity {
     Button btnShow;
@@ -22,16 +25,31 @@ public class ShowMessageActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //region AlertDialog
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(ShowMessageActivity.this);
+//                AlertDialog.Builder builder = new AlertDialog.Builder(ShowMessageActivity.this);
 
 //                builder.setTitle("Greetings");
 //                builder.setMessage("Salam");
 //                builder.setIcon(android.R.drawable.ic_dialog_info);
 //                builder.show();
 
-                builder.setTitle("Greetings")
-                        .setMessage("Salam")
-                        .setIcon(android.R.drawable.ic_dialog_alert).show();
+//                builder.setTitle("Greetings")
+//                        .setMessage("Salam")
+//                        .setIcon(android.R.drawable.ic_dialog_alert).show();
+
+                //endregion
+
+                //region Snackbar
+
+                Snackbar snackbar = Snackbar.make(btnShow, "Salam", Snackbar.LENGTH_INDEFINITE);
+
+                snackbar.setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ShowMessageActivity.this, "GOOD", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                snackbar.show();
 
                 //endregion
             }

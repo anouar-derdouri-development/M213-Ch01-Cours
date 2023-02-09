4af68e9ad6991767aaab608ba6fe0af40aa01dcc
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-02-09 19:19:06 +0000

    Add addTextChangedListener method

--- app/src/main/java/com/example/m213_ch01_cours/TextChangedListenerActivity.java
+++ app/src/main/java/com/example/m213_ch01_cours/TextChangedListenerActivity.java
@@ -3,12 +3,39 @@ package com.example.m213_ch01_cours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TextChangedListenerActivity extends AppCompatActivity {
+    EditText etMessage;
+    TextView tvMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_changed_listener);
+
+        etMessage = findViewById(R.id.etMessage);
+        tvMessage = findViewById(R.id.tvMessage);
+
+        etMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+//                tvMessage.setText(etMessage.getText());
+                tvMessage.setText(s);
+            }
+        });
     }
 }

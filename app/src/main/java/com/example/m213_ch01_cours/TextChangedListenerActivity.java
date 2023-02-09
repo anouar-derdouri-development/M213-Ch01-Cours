0857bcf4581bd048cee0fa0184deb886371a7906
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-02-09 19:00:01 +0000

    Create TextChangedListenerActivity

---
+++ app/src/main/java/com/example/m213_ch01_cours/TextChangedListenerActivity.java
@@ -0,0 +1,14 @@
+package com.example.m213_ch01_cours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextChangedListenerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_changed_listener);
+    }
+}
